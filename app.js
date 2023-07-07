82d2030997f474f23ccabf9ad64830b7671271e5
^薄荷布丁^ <81958172+SkyAerope@users.noreply.github.com>
2023-07-07 16:54:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require('express');
 const mysql = require('mysql2');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 3306;
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASS = process.env.DB_PASS || 'root';
+const DB_NAME = process.env.DB_NAME || 'test';
+const secretKey = process.env.secretKey || 'your_secret_key';
+const username = process.env.user || 'user';
+const password = process.env.pass || 'pass';
 app.use(bodyParser.json());
 
 const pool = mysql.createPool({
@@ -64,7 +73,7 @@ app.post('/api/login', (req, res) => {
   const { user, pass } = req.body;
 
   // 在实际应用中，你可以根据自己的用户存储方式（如数据库）来验证凭证
-  if (user === user && pass === pass) {
+  if (user === username && pass === password) {
     // 用户凭证验证成功
     const user = { id: 1, username: 'your_username' };
     const token = jwt.sign(user, secretKey);
